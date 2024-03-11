274f744aeae3f1987eb152c43ac0045ca5e3828a
Revprm <revy.pramana@gmail.com>
2024-03-11 19:57:12 +0700

    Initial Commit

---
+++ Codeforces/PetyaAndStrings.cpp
@@ -0,0 +1,52 @@
+// Pragma
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC num("avx2,bmi,bmi2,lzcnt,popcnt")
+// Header
+#include <bits/stdc++.h>
+
+using namespace std;
+// Define
+#define FOR(i, n) for(i = 0; i < n; i++)
+#define FORR(i, n) for(i = 0; i <= n; i++)
+#define Fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define se second
+#define fi first
+#define lb lower_bound
+#define mp make_pair
+#define pb push_back
+#define el '\n'
+// typedef
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef deque<int> dqi;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef unordered_map<int, int> umii;
+typedef unordered_set<int, int> usii;
+typedef map<int,int> mii;
+typedef map<char, int> mci;
+typedef map<char, ll> mcll;
+typedef set<int> seti;
+typedef multiset<int> msi;
+// const
+const double pi = acos(-1);
+const ll modll = 1e9+7ll;
+int main(){
+    Fast
+    string a, b;
+    cin >> a;
+    cin >> b;
+    transform(a.begin(), a.end(), a.begin(), ::tolower);
+    transform(b.begin(), b.end(), b.begin(), ::tolower);
+    // cout << a << el << b << el;
+    if (a.compare(b) > 0)cout << 1 << el;
+    else if (a.compare(b) < 0) cout << -1 << el;
+    else cout << 0 << el;
+    return 0;
+}
---
+++ Codeforces/RemoveSmallest.cpp
@@ -0,0 +1,92 @@
+// Pragma
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC num("avx2,bmi,bmi2,lzcnt,popcnt")
+// Header
+#include <bits/stdc++.h>
+using namespace std;
+// Define
+#define FOR(i, n) for (i = 0; i < n; i++)
+#define FORR(i, n) for (i = 0; i <= n; i++)
+#define Fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define se second
+#define fi first
+#define lb lower_bound
+#define mp make_pair
+#define pb push_back
+#define el '\n'
+// typedef
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef deque<int> dqi;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<char, int> umci;
+typedef unordered_set<int, int> usii;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+typedef map<char, ll> mcll;
+typedef set<int> seti;
+typedef multiset<int> msi;
+// const
+const double pi = acos(-1);
+const ll modll = 1e9 + 7ll;
+
+ll gcd(ll a, ll b){
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+ll modex(ll exp){
+    ll base = 2;
+    ll res = 1;
+    while (exp > 0){
+        if (exp & 1){
+            res = (res * base) % modll;
+        }
+        base = (base * base) % modll;
+        exp >>= 1;
+    }
+    return res % modll;
+}
+
+void aqua(){
+    int n, i, j, ans = 0; cin >> n;
+    int a[n];
+    bool chk[n];
+    memset(chk, 1, sizeof(chk));
+    for (i = 0; i < n; i++) cin >> a[i];
+    sort(a, a + n);
+    
+    // for (i = 0; i < n; i++) cout << a[i] << " ";
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++) {     
+            if (i == j) continue;
+            else if (abs(a[i] - a[j]) <= 1 && chk[i]){
+                a[i] = 0;
+                chk[i] = 0;
+            }
+        }
+    for (i = 0; i < n; i++) if (!chk[i]) ans++;
+    
+    // cout << ans << el;
+    if (n - ans <= 1) cout << "YES" << el;
+    else cout << "NO" << el;
+    
+}
+
+int main(){
+    Fast 
+    int t = 1;
+    cin >> t;
+    while (t--){
+        aqua();
+    }
+    return 0;
+}
